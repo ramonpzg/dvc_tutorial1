efb5053081d7393feceb682d876c3399cbaf4406
Ramon Perez <ramonpzg@protonmail.com>
2021-10-09 15:49:46 +1100

    Testing LGBM

--- src/train.py
+++ src/train.py
@@ -1,11 +1,11 @@
 import os, pickle, sys
 import numpy as np, pandas as pd
-import xgboost as xgb
+import lightgbm as lgb
 
 # params = yaml.safe_load(open("params.yaml"))["train"]
 
 input_data = sys.argv[1]
-output = os.path.join('models', 'xgb_model.pkl')
+output = os.path.join('models', 'lgbm_model.pkl')
 seed = 42
 n_est = 100
 
@@ -15,7 +15,7 @@ X_train = pd.read_csv(input_data)
 y_train = X_train.pop('rented_bike_count')
 
 
-rf = xgb.XGBRFRegressor(n_estimators=n_est, seed=seed)
+rf = lgb.LGBMRegressor(n_estimators=n_est, random_state=seed)
 
 rf.fit(X_train.values, y_train.values)
 
